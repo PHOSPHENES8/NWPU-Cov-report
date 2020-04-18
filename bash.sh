2888b2e49a2a460b854511ee4e87c10f9c80a0ae
Yikun Wang <49888377+PHOSPHENES8@users.noreply.github.com>
2020-04-18 12:28:38 +0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+curl --location --request POST 'http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp' \
+--header 'Cookie: UUKey=; eai-sess=' \
+--form '''dqszwz''= ''2'',' \
+--form '''j15tsfqw''= ''1'',' \
+--form '''j15tjcg''= ''1'',' \
+--form '''j15tnjssfjc''= ''1'',' \
+--form '''jtdtwfw''= ''1'',' \
+--form '''nhjsywyszz''= ''1'',' \
+--form '''nhjsdqjkzt''= ''1'',' \
+--form '''nhjssfzzgl''= ''1'',' 
